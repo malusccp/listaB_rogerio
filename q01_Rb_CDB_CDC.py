8b84f5c821735bff7973cc32405882fa2c5259b0
malusccp <luizamorais2007@gmail.com>
2025-04-09 22:03:43 -0300

    Update q01_Rb_CDB_CDC.py

--- q01_Rb_CDB_CDC.py
+++ q01_Rb_CDB_CDC.py
@@ -8,14 +8,12 @@ def CDC(valor_emprestado, taxa_mensal, tempo_meses):
   parcelas_CDC = montante_CDC / tempo_meses 
   juros_emprestimo = montante_CDC - valor_emprestado
   return montante_CDC, parcelas_CDC, juros_emprestimo
-# SIMULADOR CDB
 print('SIMULADOR CDB')
 valor_investido = float(input('Digite o valor a ser investido em R$: '))
 taxa_anual = float(input('Digite a taxa de juros ao ano em %: '))
 tempo_anos = int(input('Digite o tempo em anos: '))
 montante_CDB = CDB(valor_investido, taxa_anual, tempo_anos)
 print(f'O montante do investimento será igual a R${montante_CDB: .2f}')
-# SIMULADOR CDC
 print('SIMULADOR CDC')
 valor_emprestado = valor_investido
 taxa_mensal = float(input('Digite a taxa de juros mensal em %: '))
@@ -24,7 +22,6 @@ montante_CDC, parcelas_CDC, juro_emprestimo = CDC(valor_emprestado, taxa_mensal,
 print(f'O montante do empréstimo será igual a R${montante_CDC: .2f}')
 print(f'O cliente irá pagar ao banco correspondente a juros um total de R${juro_emprestimo: .2f}')
 print(f'Cada parcela corresponderá a um valor de R${parcelas_CDC: .2f}')
-# Lucro do Banco
 print('LUCRO DO BANCO')
 juro_investidores = montante_CDB - valor_investido
 lucro_banco = juro_emprestimo - juro_investidores
